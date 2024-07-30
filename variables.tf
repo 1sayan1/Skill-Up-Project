1e3e06e596289c2c363ce11116bad6bbf6a8db61
1sayan1 <sayan.dutta32@gmail.com>
2024-07-30 11:29:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,25 +1,26 @@
+variable "environment" {
+  description = "The environment to deploy (dev, int, prod)"
+  type        = string
+}
+
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
-  default     = "my-rg"
 }
 
 variable "location" {
   description = "Name of the region"
   type        = string
-  default     = "westus"
 }
 
 variable "vm_size" {
   description = "Size of the VM"
   type        = string
-  default     = "Standard_B1s"
 }
 
 variable "admin_username" {
   description = "Admin Username"
   type        = string
-  default     = "Sayan"
 }
 
 variable "admin_password" {
