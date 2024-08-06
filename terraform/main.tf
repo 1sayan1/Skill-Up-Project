f924c0a188614fa36ffc7d0bff07f3b74550891e
Sayan Dutta <sayandutta@Sayans-Laptop.local>
2024-08-06 15:07:05 +0530

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,38 +2,6 @@ provider "azurerm" {
   features {}
 }
 
-variable "resource_group_name" {
-  description = "The name of the resource group."
-  type        = string
-}
-
-variable "location" {
-  description = "The Azure region where resources will be created."
-  type        = string
-}
-
-variable "prefix" {
-  description = "Prefix for resource names."
-  type        = string
-}
-
-variable "admin_username" {
-  description = "The admin username for the VMs."
-  type        = string
-}
-
-variable "admin_password" {
-  description = "The admin password for the VMs."
-  type        = string
-}
-
-variable "environments" {
-  description = "A map of environments with SSH public key paths."
-  type = map(object({
-    ssh_public_key_path = string
-  }))
-}
-
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
